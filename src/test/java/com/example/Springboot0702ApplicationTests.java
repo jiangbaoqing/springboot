e73434b03bfd088d971439b2ce15f1bfc738de66
jiangbq <jiangbq@evecom.net>
2022-07-07 21:54:03 +0800

    commit test

---
+++ src/test/java/com/example/Springboot0702ApplicationTests.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot0702ApplicationTests {
+
+    private RestHighLevelClient
+
+    @Test
+    void contextLoads() {
+    }
+
+    @BeforeEach
+    void setUp() {
+        HttpHost
+
+    }
+
+    @AfterAll
+    static void afterAll() {
+
+    }
+
+    void testCreateIndexBy()
+
+}
